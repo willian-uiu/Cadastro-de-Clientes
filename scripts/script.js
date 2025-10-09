008eb63366fd0a29d69acdb5d132cfce68b0f5b3
Willian <williansilvamamede@gmail.com>
2025-10-08 22:44:45 -0300

    closes #8; Realizado a requisição ao sair do campo CEP

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
-let cepDoc = document.getElementById("cepValidation").value;
-function buscaCep() {
+// CÓDIGO QUE VALIDA SE OS CAMPOS ESTÃO PREENCHIDOS
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
+
+
+
+function buscaCEP() {
+    let cepDoc = document.getElementById("cepValidation").value;
     $.getJSON(`https://viacep.com.br/ws/${cepDoc}/json`, (response) => {
         document.getElementById("addressValidation").value = response.logradouro;
         document.getElementById("brotherHoodValidation").value = response.bairro;
@@ -8,5 +31,3 @@ function buscaCep() {
     }
     )
 }
-
-buscaCep();

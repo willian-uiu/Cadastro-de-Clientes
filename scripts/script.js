44169fad987201be1c30da50e36ae26c6cd5fca2
Willian <williansilvamamede@gmail.com>
2025-10-08 22:31:03 -0300

    Closes #6; Mostrar Requisição nos campos do formulário

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
 let cepDoc = document.getElementById("cepValidation").value;
-function buscaCep(cep) {
-    cep = cepDoc;
-    $.getJSON(`https://viacep.com.br/ws/${cep}/json`, (response) => console.log(response));
+function buscaCep() {
+    $.getJSON(`https://viacep.com.br/ws/${cepDoc}/json`, (response) => {
+        document.getElementById("addressValidation").value = response.logradouro;
+        document.getElementById("brotherHoodValidation").value = response.bairro;
+        document.getElementById("cityValidation").value = response.localidade;
+        document.getElementById("ufValidation").value = response.uf;
+    }
+    )
 }
 
 buscaCep();
